dd9d76658e8feeb9d2bc245d62356a844053756b
niszco <nico.marszalek@gmail.com>
2024-01-27 10:50:57 +0100

    feat: More small fixes

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -18,15 +20,13 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- src/Enum/Color.php
+++ src/Enum/Color.php
@@ -7,4 +7,24 @@ enum Color: string
     case ORANGE = "Orange";
     case NOIR = "Noir";
     case GRIS_CLAIR = "Gris clair";
+    case ROUGE = "Rouge";
+    case VERT = "Vert";
+    case BLEU = "Bleu";
+    case JAUNE = "Jaune";
+    case VIOLET = "Violet";
+    case ROSE = "Rose";
+    case MARRON = "Marron";
+    case BLANC = "Blanc";
+    case GRIS_FONCÉ = "Gris foncé";
+    case TURQUOISE = "Turquoise";
+    case SAUMON = "Saumon";
+    case BEIGE = "Beige";
+    case ARGENT = "Argent";
+    case DORÉ = "Doré";
+    case INDIGO = "Indigo";
+    case AZUR = "Azur";
+    case BRUN = "Brun";
+    case CYAN = "Cyan";
+    case CUSTOM = "Personnalisé";
+    case COVERING = "Covering";
 }
